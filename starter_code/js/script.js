f94b0bd3c53a39871f20fac26effcd943d7f482e
taty37 <tpiercy37@gmail.com>
2018-03-27 19:04:38 -0700

    Fixed code
    conversion of the attr handle

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -13,17 +13,17 @@ $(document).ready(function(){
 		console.log(city);
 //use the city value to find out if when someone subnmits different values, it changes to the background image based on the city
 		if (city === 'New York' || city === 'NYC' || city === 'New York City' || city === 'new york' || city === 'nyc' || city === 'new york city') {
-			$('body').removeClass('starter-image').addClass('nyc');
+			$('body').attr("class", 'nyc');
 		} else if (city === 'San Francisco' || city === 'SF' || city === 'Bay Area' || city === 'san francisco' || city === 'sf' || city === 'bay area') {
-			$('body').removeClass('starter-image').addClass('sf');
+			$('body').attr("class", 'sf');
 		} else if (city === 'Los Angeles' || city === 'LA' || city === 'LAX' || city === 'los angeles' || city === 'la' || city === 'lax') {
-			$('body').removeClass('starter-image').addClass('la');
+			$('body').attr("class", 'la');
 		} else if (city === 'Austin' || city === 'ATX' || city === 'austin' || city === 'atx') {
-			$('body').removeClass('starter-image').addClass('austin');
+			$('body').attr("class", 'austin');
 		} else if (city === 'Sydney' || city === 'SYD' || city === 'sydney' || city === 'syd') {
-			$('body').removeClass('starter-image').addClass('sydney');
+			$('body').attr("class", 'sydney');
 		} else {
-			$('body').addClass('starter-image');	
+			$('body').attr("class", 'starter-image' )
 		}
 }
 	});
